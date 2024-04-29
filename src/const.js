4088298dfeae8f8382ba40406c44e4a6932fc36e
Egor <egorviliavin@gmail.com>
2024-04-29 18:20:31 +0300

    Fix: Переносит данные в const.js

--- src/const.js
+++ src/const.js
@@ -1,79 +1,102 @@
 export const TYPE_EVENTS = [
   {
-    value: 'Taxi',
-    img: 'img/icons/taxi.png',
+    value: "Taxi",
+    img: "img/icons/taxi.png",
   },
   {
-    value: 'Bus',
-    img: 'img/icons/bus.png',
+    value: "Bus",
+    img: "img/icons/bus.png",
   },
   {
-    value: 'Train',
-    img: 'img/icons/train.png',
+    value: "Train",
+    img: "img/icons/train.png",
   },
   {
-    value: 'Ship',
-    img: 'img/icons/ship.png',
+    value: "Ship",
+    img: "img/icons/ship.png",
   },
   {
-    value: 'Drive',
-    img: 'img/icons/drive.png',
+    value: "Drive",
+    img: "img/icons/drive.png",
   },
   {
-    value: 'Flight',
-    img: 'img/icons/flight.png',
+    value: "Flight",
+    img: "img/icons/flight.png",
   },
   {
-    value: 'Check-in',
-    img: 'img/icons/check-in.png',
+    value: "Check-in",
+    img: "img/icons/check-in.png",
   },
   {
-    value: 'Sightseeing',
-    img: 'img/icons/sightseeing.png',
+    value: "Sightseeing",
+    img: "img/icons/sightseeing.png",
   },
   {
-    value: 'Restaurant',
-    img: 'img/icons/restaurant.png',
+    value: "Restaurant",
+    img: "img/icons/restaurant.png",
   },
 ];
 
 export const BUTTON_OFFERS = [
   {
-    title: 'Add luggage',
-    price: '50',
-    value: 'luggage',
+    title: "Add luggage",
+    price: "50",
+    value: "luggage",
     isChecked: true,
   },
   {
-    title: 'Switch to comfort',
-    price: '80',
-    value: 'comfort',
+    title: "Switch to comfort",
+    price: "80",
+    value: "comfort",
     isChecked: true,
   },
   {
-    title: 'Add meal',
-    price: '15',
-    value: 'meal',
+    title: "Add meal",
+    price: "15",
+    value: "meal",
     isChecked: false,
   },
   {
-    title: 'Choose seats',
-    price: '5',
-    value: 'meal',
+    title: "Choose seats",
+    price: "5",
+    value: "meal",
     isChecked: false,
   },
   {
-    title: 'Travel by train',
-    price: '40',
-    value: 'train',
+    title: "Travel by train",
+    price: "40",
+    value: "train",
     isChecked: false,
   },
 ];
 
 export const DESTINATION_IMAGES = [
-  'img/photos/1.jpg',
-  'img/photos/2.jpg',
-  'img/photos/3.jpg',
-  'img/photos/4.jpg',
-  'img/photos/5.jpg',
+  "img/photos/1.jpg",
+  "img/photos/2.jpg",
+  "img/photos/3.jpg",
+  "img/photos/4.jpg",
+  "img/photos/5.jpg",
+];
+
+export const SORT_VALUES = [
+  {
+    value: "Day",
+    isChecked: true,
+  },
+  {
+    value: "Event",
+    isDisabled: true,
+  },
+  {
+    value: "Time",
+    isChecked: false,
+  },
+  {
+    value: "Price",
+    isChecked: false,
+  },
+  {
+    value: "Offers",
+    isDisabled: true,
+  },
 ];
--- src/view/trip-form-sort-view.js
+++ src/view/trip-form-sort-view.js
@@ -1,40 +1,24 @@
-import { createElement } from '../render';
+import { createElement } from "../render";
 
-const SORT_VALUES = [
-  {
-    value: 'Day',
-    isChecked: true,
-  },
-  {
-    value: 'Event',
-    isDisabled: true,
-  },
-  {
-    value: 'Time',
-    isChecked: false,
-  },
-  {
-    value: 'Price',
-    isChecked: false,
-  },
-  {
-    value: 'Offers',
-    isDisabled: true,
-  },
-];
+import { SORT_VALUES } from "../const";
 
-const createSortValuesElement = () => SORT_VALUES.map((item) => `<div class="trip-sort__item  trip-sort__item--${item.value.toLocaleLowerCase()}">
+const createSortValuesElement = () =>
+  SORT_VALUES.map(
+    (
+      item
+    ) => `<div class="trip-sort__item  trip-sort__item--${item.value.toLocaleLowerCase()}">
                 <input id="sort-${item.value.toLocaleLowerCase()}" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" item.value="sort-${item.value.toLocaleLowerCase()}"
-                ${item.isChecked ? 'checked' : ''}
-                ${item.isDisabled ? 'disabled' : ''}>
+                ${item.isChecked ? "checked" : ""}
+                ${item.isDisabled ? "disabled" : ""}>
                 <label class="trip-sort__btn" for="sort-${item.value.toLocaleLowerCase()}">${
-  item.value
-}</label>
-            </div>`);
+      item.value
+    }</label>
+            </div>`
+  );
 
 const tripEventsBoardViewTemplate = () => `
     <form class="trip-events__trip-sort trip-sort" action="#" method="get">
-      ${createSortValuesElement().join('')}
+      ${createSortValuesElement().join("")}
     </form>
 `;
 
