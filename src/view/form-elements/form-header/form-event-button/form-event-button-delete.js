f8ce08be6edf9d0bbf133aaebe3d7f54d8ea5a92
Egor <egorviliavin@gmail.com>
2024-04-30 16:00:15 +0300

    Fix: Исправляет ошибки линтера

--- src/view/form-elements/form-header/form-event-button/form-event-button-delete.js
+++ src/view/form-elements/form-header/form-event-button/form-event-button-delete.js
@@ -1,8 +1,6 @@
 import { FormType } from '../../../../const';
 export const createFormEventButtonDeleteTemplate = (formTypeSelect) => {
-  const getRenderByFormType = () => {
-    return `${formTypeSelect === FormType.FORM_ADD ? 'Cancel' : 'Delete'}`;
-  };
+  const getRenderByFormType = () => `${formTypeSelect === FormType.FORM_ADD ? 'Cancel' : 'Delete'}`;
 
   return `<button class="event__reset-btn" type="reset">${getRenderByFormType()}</button>`;
 };
