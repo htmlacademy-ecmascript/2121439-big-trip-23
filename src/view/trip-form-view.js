55416e64d8cf27aa3e770fb469ea678308c0e221
Egor <egorviliavin@gmail.com>
2024-05-14 13:56:24 +0300

    Fix: иправляет ошибки линтера

--- src/view/trip-form-view.js
+++ src/view/trip-form-view.js
@@ -1,32 +1,30 @@
-import { createElement } from '../render';
+import AbstractView from '../framework/view/abstract-view';
 import { FormType } from '../const';
-
 import { createFormHeaderTemplate } from './form-elements/form-header/form-header';
 import { createFormEventDetailsTemplate } from './form-elements/form-event-details/form-event-details';
 
 const renderByTypeFormElement = (
   formTypeSelect,
   pointDestinations,
-  pointOffers
+  pointOffers,
+  point,
+  allOffers
 ) => {
   if (formTypeSelect === FormType.FORM_EDIT) {
     return `<form class="event event--edit" action="#" method="post">
-    ${createFormHeaderTemplate(formTypeSelect, pointOffers)}
-    ${createFormEventDetailsTemplate(
-      formTypeSelect,
-      pointDestinations,
-      pointOffers
-    )}
+    ${createFormHeaderTemplate(formTypeSelect, pointOffers, point)}
+    ${createFormEventDetailsTemplate(pointDestinations, allOffers, point)}
     </form>
 `;
   } else if (formTypeSelect === FormType.FORM_ADD) {
     return `<form class="event event--edit" action="#" method="post">
     ${createFormHeaderTemplate(formTypeSelect, pointOffers)}
     ${createFormEventDetailsTemplate(
-      formTypeSelect,
-      pointDestinations,
-      pointOffers
-    )}
+    formTypeSelect,
+    pointDestinations,
+    allOffers,
+    point
+  )}
     </form>
 `;
   }
@@ -35,32 +33,68 @@ const renderByTypeFormElement = (
 const createTripFormTemplate = (
   formTypeSelect,
   pointDestinations,
-  pointOffers
-) => renderByTypeFormElement(formTypeSelect, pointDestinations, pointOffers);
+  pointOffers,
+  point,
+  allOffers
+) =>
+  `<li class="trip-events__item">${renderByTypeFormElement(
+    formTypeSelect,
+    pointDestinations,
+    pointOffers,
+    point,
+    allOffers
+  )}</li>`;
+export default class TripFormView extends AbstractView {
+  #formTypeSelect = null;
+  #pointDestinations = null;
+  #pointOffers = null;
+  #point = null;
+  #handleClickEdit = null;
+  #handleFormSubmit = null;
+  #rollupButton = null;
+  #allOffers = null;
 
-export default class TripFormView {
-  constructor(formTypeSelect, pointDestinations, pointOffers) {
-    this.formTypeSelect = formTypeSelect;
-    this.pointDestinations = [...pointDestinations];
-    this.pointOffers = [...pointOffers];
+  constructor({
+    formType: formTypeSelect,
+    destinations: pointDestinations,
+    pointOffers: pointOffers,
+    point: point,
+    onEditClick: onEditClick,
+    onFormSubmit: onFormSubmit,
+    allOffers: allOffers,
+  }) {
+    super();
+    this.#formTypeSelect = formTypeSelect;
+    this.#pointDestinations = [...pointDestinations];
+    this.#pointOffers = [...pointOffers];
+    this.#point = point;
+    this.#allOffers = allOffers;
+    this.#handleClickEdit = onEditClick;
+    this.#handleFormSubmit = onFormSubmit;
+    this.#rollupButton = this.element.querySelector('.event__rollup-btn');
+    this.#rollupButton.addEventListener('click', this.#onClickEdit);
+    this.element
+      .querySelector('form')
+      .addEventListener('submit', this.#onFormSubmit);
   }
 
-  getTemplate() {
+  get template() {
     return createTripFormTemplate(
-      this.formTypeSelect,
-      this.pointDestinations,
-      this.pointOffers
+      this.#formTypeSelect,
+      this.#pointDestinations,
+      this.#pointOffers,
+      this.#point,
+      this.#allOffers
     );
   }
 
-  getElement() {
-    if (!this.element) {
-      this.element = createElement(this.getTemplate());
-    }
-    return this.element;
-  }
+  #onClickEdit = (evt) => {
+    evt.preventDefault();
+    this.#handleClickEdit();
+  };
 
-  removeElement() {
-    this.element = null;
-  }
+  #onFormSubmit = (evt) => {
+    evt.preventDefault();
+    this.#handleFormSubmit();
+  };
 }
